6949d3971f77942521cebb9ef91001e8d5087c39
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2020-07-13 14:59:29 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,17 +28,10 @@ RSS_FEED_URL="https://$GITHUB_ACTOR.github.io/$REPO_NAME/feed.xml"
     echo '<rss version="2.0">'
     printf "<channel>\n<title>Help Wanted</title>\n<description>Help Wanted Issues</description>\n<link>$RSS_FEED_URL</link>\n"
 
-    echo $ORGS
-
     for ORG in $(echo $ORGS | tr ',' ' ')
     do
-    
-        echo;echo;echo $ORG;echo;echo;
-    
         #Plumb
         STOP=$(curl -k -v -u :$TOKEN "https://api.github.com/users/$ORG/repos" -o /dev/null 2>&1 | tr [:punct:] ' ' | awk '/next/ { print $21 }')
-
-        echo "STOP: $STOP"
 
         for PAGE in $(seq 1 $STOP)
         do
@@ -46,15 +39,15 @@ RSS_FEED_URL="https://$GITHUB_ACTOR.github.io/$REPO_NAME/feed.xml"
             
             echo "SNATCH https://api.github.com/users/$ORG/repos?page=$PAGE"
             
-            #curl -k -s -u :$TOKEN "https://api.github.com/users/$ORG/repos?page=$PAGE" | jq '.[] | "\(.open_issues) \(.full_name)"' | tr -d '"' | awk '$1 > 0 { print $2}' | while read ISSUED
-            #do
-            #    # Only tell me about repos that contain languages I use
-            #    curl -k -s -u :$TOKEN "https://api.github.com/repos/$ISSUED/languages" | jq . | egrep -qi "$LANGUAGES" && (
-            #        curl -k -s -u :$TOKEN "https://api.github.com/repos/$ISSUED/issues" |\
-            #          jq '.[] | "\(.updated_at)¡\(.labels[].name)¡\(.title)¡\(.html_url)¡\(.body)"' | awk -F"¡" '/help wanted/ { gsub(/[\"|\-|T|:|Z]/, " ", $1); if ((systime()-"'$CUTOFFDATE'")<mktime($1)) print $3"¡"$4"¡"$5 }' |\
-            #          awk -F"¡" '{ gsub(/\\n/, "<br\/>", $3); print "<item>\n\t<title>"$1"</title>\n\t<link>"$2"</link>\n\t<description><![CDATA["$3" ]]></description>\n</item>\n" }' 2>/dev/null | perl -e 'while(<>){$_=~s/\\r//g;print}'
-            #    )
-            #done
+            curl -k -s -u :$TOKEN "https://api.github.com/users/$ORG/repos?page=$PAGE" | jq '.[] | "\(.open_issues) \(.full_name)"' | tr -d '"' | awk '$1 > 0 { print $2}' | while read ISSUED
+            do
+                # Only tell me about repos that contain languages I use
+                curl -k -s -u :$TOKEN "https://api.github.com/repos/$ISSUED/languages" | jq . | egrep -qi "$LANGUAGES" && (
+                    curl -k -s -u :$TOKEN "https://api.github.com/repos/$ISSUED/issues" |\
+                      jq '.[] | "\(.updated_at)¡\(.labels[].name)¡\(.title)¡\(.html_url)¡\(.body)"' | awk -F"¡" '/help wanted/ { gsub(/[\"|\-|T|:|Z]/, " ", $1); if ((systime()-"'$CUTOFFDATE'")<mktime($1)) print $3"¡"$4"¡"$5 }' |\
+                      awk -F"¡" '{ gsub(/\\n/, "<br\/>", $3); print "<item>\n\t<title>"$1"</title>\n\t<link>"$2"</link>\n\t<description><![CDATA["$3" ]]></description>\n</item>\n" }' 2>/dev/null | perl -e 'while(<>){$_=~s/\\r//g;print}'
+                )
+            done
         done
     done
 
