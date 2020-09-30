4026d752f1d9bc68fd1d8572294b17e1e9b2ee9c
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2020-09-30 07:45:29 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,10 +35,28 @@ do
 
     for PAGE in $(seq 1 $STOP)
     do
-    
-        echo "TICKLE: https://api.github.com/users/$ORG/repos?page=$PAGE"
+        curl -s -u :$TOKEN "https://api.github.com/users/$ORG/repos?page=$PAGE" | jq '.[] | "\(.open_issues) \(.full_name)"' | awk '$1 != "\"0" { gsub(/"/, ""); print $NF }' | while read I
+        do
+            curl -s -u :$TOKEN "https://api.github.com/repos/$I/languages" | jq . | egrep -qi "$LANGUAGES" && (
+                curl -s -u :$TOKEN "https://api.github.com/repos/$I/issues" | jq '.[] | "\(.updated_at)¡\(.labels[].name)¡\(.title)¡\(.html_url)¡\(.body)"' | egrep -i "$LABELS" | while read RAW
+                do
+                    #echo "$RAW"
+                        THEN=$(date -d $(echo "$RAW" | awk -F"¡" '{ gsub(/"/, ""); print $1 }') +%s )
+                        DIFF=$(($(date +%s)-$THEN))
 
-        curl -s -u :$TOKEN "https://api.github.com/users/$ORG/repos?page=$PAGE" | jq .[]
+                        if [[ $DIFF -ge $CUTOFFDATE ]]
+                        then
+                            true
+                        else
+                            LABELS=$(echo   "$RAW" | awk -F"¡" '{ print $2 }')
+                            TITLE=$(echo    "$RAW" | awk -F"¡" '{ print $3 }')
+                            URL=$(echo      "$RAW" | awk -F"¡" '{ print $4 }')
+                            BODY=$(echo     "$RAW" | awk -F"¡" '{ print $5 }' | pandoc | sed -e 's/rn/<br>/g')
+                            printf "<item>\t<title>$TITLE</title>\n\t<link>$URL</link>\n\t<description><![CDATA[ $BODY ]]></description>\n</item>\n"
+                        fi
+                done
+            )
+            done
 
     done
 done
