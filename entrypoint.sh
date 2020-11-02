01c4bb11d0df45d68c46a8ed9d69fd2965853036
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2020-11-02 15:08:13 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -65,12 +65,12 @@ RSS_FEED_URL="https://$GITHUB_ACTOR.github.io/$REPO_NAME/feed.xml"
                         
                         BODY=$(curl -s -u :$TOKEN "https://api.github.com/repos/$I/issues/$ID" | jq .body| sed -e 's/^"//' | sed -e 's/"$//'| xargs -0 printf | pandoc --wrap=preserve)
                         
-                        #if [[ $PRed -gt 0 ]]
-                        #then
-                            printf "<item>\t<title>$TITLE</title>\n\t<link>$URL</link>\n\t<description><![CDATA[ <h1 style="background-color:yellow">$PRed PRs opened</h1><pre>$BODY</pre> ]]></description>\n</item>\n" | awk '{ gsub("\014","\\f"); gsub("\010","\\b"); print }'
-                        #else
-                        #    printf "<item>\t<title>$TITLE</title>\n\t<link>$URL</link>\n\t<description><![CDATA[ <pre>$BODY</pre> ]]></description>\n</item>\n" | awk '{ gsub("\014","\\f"); gsub("\010","\\b"); print }'
-                        #fi
+                        if [[ $PRed -gt 0 ]]
+                        then
+                            printf "<item>\t<title>$TITLE</title>\n\t<link>$URL</link>\n\t<description><![CDATA[ <h1 style=\"background-color:yellow\">$PRed PRs opened</h1><pre>$BODY</pre> ]]></description>\n</item>\n" | awk '{ gsub("\014","\\f"); gsub("\010","\\b"); print }'
+                        else
+                            printf "<item>\t<title>$TITLE</title>\n\t<link>$URL</link>\n\t<description><![CDATA[ <h1 style=\"background-color:yellow\">0 PRs opened</h1><pre>$BODY</pre> ]]></description>\n</item>\n" | awk '{ gsub("\014","\\f"); gsub("\010","\\b"); print }'
+                        fi
                     fi
                 done
                 )
