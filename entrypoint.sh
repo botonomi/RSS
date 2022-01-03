3c2b6b4eb5db0eba67cbbd553776427e87a2f074
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2022-01-03 13:49:49 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -68,7 +68,7 @@ RSS_FEED_URL="https://$GITHUB_ACTOR.github.io/$REPO_NAME/feed.xml"
                                 #-PRed=$(curl -s -u :$TOKEN "https://github.com/pulls?q=is%3Apr+user%3A"$ORG"+%23"$ID | jq .total_count)
                         
                                 BODY=$(curl -s -u :$TOKEN "https://api.github.com/repos/$I/issues/$ID" | jq .body | sed -e 's/^"//' | sed -e 's/"$//' | xargs -0 echo -e | pandoc --wrap=preserve)
-                                echo "BODYSLAM: ${#BODY}"
+                                #echo "BODYSLAM: ${#BODY}"
 
                                 if [[ ${#BODY} -gt 1000 ]]
                                 then
